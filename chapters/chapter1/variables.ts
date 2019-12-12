439e8a293820a43e98f878de3e561f82957a016e
Sai Kumar Reddy <saikumar@fftech.com>
2019-12-12 19:37:50 +0530

    commit

--- chapters/chapter1/variables.ts
+++ chapters/chapter1/variables.ts
@@ -1,3 +1,13 @@
 var globalVar = "I am Global Var here";
 
+globalVar = "I Can change Any where.";
 console.log(globalVar);
+
+let localVar = "I am Local Var here";
+localVar = "I get Change only block level scope";
+
+console.log(localVar);
+
+const constVar = "I am Constant Var here";
+
+console.log(constVar);
