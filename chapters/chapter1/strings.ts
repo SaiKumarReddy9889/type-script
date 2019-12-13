2c30322c166b451f02b26ce340425cf385bc2164
Sai Kumar Reddy <saikumar@fftech.com>
2019-12-13 12:29:07 +0530

    commit

--- chapters/chapter1/strings.ts
+++ chapters/chapter1/strings.ts
@@ -1,1 +1,8 @@
+var imstrr = "I am the king of my kingdom";
+console.log(imstrr.replace("king", "queen"));
 
+var email = "narendrareddy@gmail.com";
+
+var newMail = email.split("@")[0];
+
+console.log(newMail);
--- chapters/chapter1/variables.ts
+++ chapters/chapter1/variables.ts
@@ -1,13 +1,19 @@
-var globalVar = "I am Global Var here";
+// var globalVar = "I am Global Var here";
 
-globalVar = "I Can change Any where.";
-console.log(globalVar);
+// globalVar = "I Can change Any where.";
+// console.log(globalVar);
 
-let localVar = "I am Local Var here";
-localVar = "I get Change only block level scope";
+// let localVar = "I am Local Var here";
+// localVar = "I get Change only at block level scope";
 
-console.log(localVar);
+// console.log(localVar);
 
 const constVar = "I am Constant Var here";
+
+// constVar = "i cannot be changed";
+
+const arr = ["bob", "foo", "bar"];
+
+console.log(arr);
 
 console.log(constVar);
