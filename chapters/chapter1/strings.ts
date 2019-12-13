91e50125c8b7095c8f1a62408a49bdd8803ae28b
Sai Kumar Reddy <saikumar@fftech.com>
2019-12-13 13:16:51 +0530

    commit

--- chapters/chapter1/strings.ts
+++ chapters/chapter1/strings.ts
@@ -1,7 +1,7 @@
 var imstrr = "I am the king of my kingdom";
 console.log(imstrr.replace("king", "queen"));
 
-var email = "narendrareddy@gmail.com";
+var email = "SaiKumarReddy.Gurram@gmail.com";
 
 var newMail = email.split("@")[0];
 
